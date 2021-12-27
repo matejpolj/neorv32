edd6cee4147f30316fe41b2455e2415355a1ab99
matejpolj <42142798+matejpolj@users.noreply.github.com>
2021-12-27 10:30:58 +0100

    pripravil do nekaj dela
    Pač zdaj zadeva toliko dela, da imam vse gpio in pwm sranje usposobljeno, da se vsaj pokaže!!!

---
+++ setups/test_more_memory/output_files/de0-nano-test-setup.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE22) Path("/mnt/5064E3264BEB0448/github/rubikscube/neorv32/setups/test_more_memory/") File("output_file_17.jic") MfrSpec(OpMask(1) SEC_Device(EPCS64) Child_OpMask(1 1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- setups/test_more_memory_3/neorv32_test_setup_bootloader.vhd
+++ setups/test_more_memory_3/neorv32_test_setup_bootloader.vhd
@@ -77,17 +77,10 @@ begin
     CLOCK_FREQUENCY              => CLOCK_FREQUENCY,   -- clock frequency of clk_i in Hz
     INT_BOOTLOADER_EN            => true,              -- boot configuration: true = boot explicit bootloader; false = boot from int/ext (I)MEM
     -- RISC-V CPU Extensions --
-	 CPU_EXTENSION_RISCV_A        => true,					 -- implement atomic extension?
-    CPU_EXTENSION_RISCV_B        => true,  			    -- implement bit-manipulation extension?
-    CPU_EXTENSION_RISCV_C        => true,  				 -- implement compressed extension?
-    CPU_EXTENSION_RISCV_E        => true,  				 -- implement embedded RF extension?
-    CPU_EXTENSION_RISCV_M        => true, 				 -- implement mul/div extension?
-    CPU_EXTENSION_RISCV_U        => true,  				 -- implement user mode extension?
-    CPU_EXTENSION_RISCV_Zfinx    => true,  				 -- implement 32-bit floating-point extension (using INT regs!)
-    CPU_EXTENSION_RISCV_Zicsr    => true,   				 -- implement CSR system?
-    CPU_EXTENSION_RISCV_Zicntr   => true,   				 -- implement base counters?
-    CPU_EXTENSION_RISCV_Zihpm    => true,  				 -- implement hardware performance monitors?
-    CPU_EXTENSION_RISCV_Zifencei => true,  				 -- implement instruction stream sync.?
+	 CPU_EXTENSION_RISCV_C        => true,              -- implement compressed extension?
+    CPU_EXTENSION_RISCV_M        => true,              -- implement mul/div extension?
+    CPU_EXTENSION_RISCV_Zicsr    => true,              -- implement CSR system?
+    CPU_EXTENSION_RISCV_Zicntr   => true,              -- implement base counters?
     -- Internal Instruction memory --
     MEM_INT_IMEM_EN              => true,              -- implement processor-internal instruction memory
     MEM_INT_IMEM_SIZE            => MEM_INT_IMEM_SIZE, -- size of processor-internal instruction memory in bytes
