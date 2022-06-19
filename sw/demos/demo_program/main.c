377b8899ee53e9bd039a713c03be046187a3c916
matejpolj <42142798+matejpolj@users.noreply.github.com>
2022-06-19 17:15:04 +0200

    Update main.c

--- sw/demos/demo_program/main.c
+++ sw/demos/demo_program/main.c
@@ -1,67 +1,13 @@
-// #################################################################################################
-// # << NEORV32 - PWM Demo Program >>                                                              #
-// # ********************************************************************************************* #
-// # BSD 3-Clause License                                                                          #
-// #                                                                                               #
-// # Copyright (c) 2021, Stephan Nolting. All rights reserved.                                     #
-// #                                                                                               #
-// # Redistribution and use in source and binary forms, with or without modification, are          #
-// # permitted provided that the following conditions are met:                                     #
-// #                                                                                               #
-// # 1. Redistributions of source code must retain the above copyright notice, this list of        #
-// #    conditions and the following disclaimer.                                                   #
-// #                                                                                               #
-// # 2. Redistributions in binary form must reproduce the above copyright notice, this list of     #
-// #    conditions and the following disclaimer in the documentation and/or other materials        #
-// #    provided with the distribution.                                                            #
-// #                                                                                               #
-// # 3. Neither the name of the copyright holder nor the names of its contributors may be used to  #
-// #    endorse or promote products derived from this software without specific prior written      #
-// #    permission.                                                                                #
-// #                                                                                               #
-// # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS   #
-// # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF               #
-// # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE    #
-// # COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,     #
-// # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE #
-// # GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED    #
-// # AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     #
-// # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED  #
-// # OF THE POSSIBILITY OF SUCH DAMAGE.                                                            #
-// # ********************************************************************************************* #
-// # The NEORV32 Processor - https://github.com/stnolting/neorv32              (c) Stephan Nolting #
-// #################################################################################################
-
-
-/**********************************************************************//**
- * @file demo_pwm/main.c
- * @author Stephan Nolting
- * @brief Simple PWM usage example.
- **************************************************************************/
-
 #include <neorv32.h>
-#include "buttons.h"
-#include "config.h"
+#include "buttons.h" // knjižnica s funkcijami za tipke
+#include "config.h" // knjižnica z GPIO konfiguracijo
 
-
-/**********************************************************************//**
- * @name User configuration
- **************************************************************************/
-/**@{*/
 /** UART BAUD rate */
 #define BAUD_RATE 19200
 /** Maximum PWM output intensity (0..255) */
 #define PWM_MAX 128
 /**@}*/
 
-
-/**********************************************************************//**
- * This program generates a simple dimming sequence for PWM channel 0,1,2.
- *
- * @note This program requires the PWM controller to be synthesized (the UART is optional).
- *
- * @return 0 if execution was successful
- **************************************************************************/
 int main() {
 
   // check if PWM unit is implemented at all
@@ -85,7 +31,7 @@ int main() {
     // check available hardware extensions and compare with compiler flags
     neorv32_rte_check_isa(0); // silent = 0 -> show message if isa mismatch
 
-    // say hello
+
     neorv32_uart0_print("PWM in tipke demo program\n");
   }
 
